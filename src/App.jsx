5904624940f35f8481e8170c9b570a48df0858fc
Carlos Eduardo Ortega Frias <eduardo.ortega@g-global.com>
2023-01-11 13:23:24 -0800

    Github pages link

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/cursos" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
